d278de88c0c04bafead7e1f65938cf08a32018ab
StivoMix <stiv2009greenwolf@gmail.com>
2026-06-05 00:59:08 +0300

    feat(runner): implement execute() orchestrator for full pipeline
    execute() function now runs the entire data poisoning pipeline end to
    end. loads a model, gets its base metrics, poisons it, gets poison
    metrics, sanitizes it, gets its sanitized metrics, and returns a proper
    instance of the RunResult class.

--- core/runner.py
+++ core/runner.py
@@ -1,43 +1,50 @@
-from models import AttackConfig, RunResult, MetricSnapshot
-from transformers import PreTrainedModel, PreTrainedTokenizerBase, AutoTokenizer, AutoModel
+from core.models import AttackConfig, RunResult, MetricSnapshot
+from transformers import PreTrainedModel, PreTrainedTokenizerBase, AutoTokenizer, AutoModelForSequenceClassification
 from datasets import Dataset, load_dataset
 from core.training import fine_tune
 from core.evaluation import capture_metrics
-from core.attacks import AttackConfig, LabelFlipAttack, Attack
+from core.attacks import LabelFlipAttack, Attack
 from core.defenses import label_noise_filter_apply
-from exceptions import NotImplementedError
+from time import perf_counter
 
-def _load_model_and_tokenizer(model_id: str) -> tuple[PreTrainedModel, PreTrainedTokenizerBase]:
+def _load_model(model_id: str) -> PreTrainedModel:
     """
-    Get a pre trained model and its corresponding tokenizer from its name.
+    Get a pre trained model by its ID from HuggingFace.
 
     Args:
         model_id (str): The name of the model.
 
     Returns:
-        tuple[PreTrainedModel, PreTrainedTokenizerBase]: A tuple containing the pulled model and its tokenizer from huggingface.
+        PreTrainedModel: The pulled model from HuggingFace.
     """
-    return AutoModel.from_pretrained(model_id), AutoTokenizer.from_pretrained(model_id)
+    return AutoModelForSequenceClassification.from_pretrained(model_id)
 
 
-def _load_dataset(
-    dataset_name: str,
-    samples: int | None = None,
-    seed: int = 0
-) -> Dataset:
+def _load_tokenizer(model_id: str) -> PreTrainedTokenizerBase:
+    """
+    Get a pre trained tokenizer base by its model ID from HuggingFace.
+
+    Args:
+        model_id (str): The name of the model.
+
+    Returns:
+        PreTrainedTokenizerBase: The pulled tokenizer base from HuggingFace.
+    """
+    return AutoTokenizer.from_pretrained(model_id)
+
+
+def _load_dataset(config: AttackConfig) -> Dataset:
     """
     Pulls samples out of a dataset from HuggingFace.
 
     Args:
-        dataset_name (str): The name of the dataset.
-        samples (int | None): Amount of samples to get of the dataset. Defaults to None. If none, returns the full dataset.
-        seed (int, optional): An RNG seed for reproducibility. Defaults to 0.
+        config (AttackConfig): User's built attack configuration.
 
     Returns:
         Dataset: Batched chunks of the pulled dataset.
     """
-    dataset: Dataset = load_dataset(dataset_name).shuffle(seed)
-    return dataset.select(range(samples)) if samples is not None else dataset
+    dataset: Dataset = load_dataset(config.dataset_name, split="train").shuffle(seed=config.seed)
+    return dataset.select(range(config.samples)) if config.samples is not None else dataset
 
 
 def _build_attack(config: AttackConfig) -> Attack:
@@ -61,8 +68,7 @@ def _build_attack(config: AttackConfig) -> Attack:
                 seed=config.seed
             )
         case _:
-            raise NotImplementedError(config.attack_type)
-
+            raise NotImplementedError(f"Action {config.attack_type} not yet implemented")
 
 def _build_defense(config: AttackConfig):
     """
@@ -78,24 +84,19 @@ def _build_defense(config: AttackConfig):
         Attack: A defense instance to be executed.
     """
     match config.defense_type:
+        case "none":
+            return None
         case "label_noise_filter":
             return label_noise_filter_apply # returning the function signature for now till remade into a class
         case _:
-            raise NotImplementedError(config.defense_type)
-
+            raise NotImplementedError(f"Action {config.defense_type} not yet implemented")
 
 def _train_and_measure(
     model: PreTrainedModel,
     tokenizer: PreTrainedTokenizerBase,
     train_ds: Dataset,
     eval_ds: Dataset,
-    text_column: str,
-    target_column: str,
-    seed: int = 0,
-    epochs: int = 1,
-    learning_rate: float = 2e-5,
-    output_dir: str = "./training_output",
-    device: str = "cpu"
+    config: AttackConfig
 ) -> tuple[PreTrainedModel, MetricSnapshot]:
     """
     Fine tune a model and measure its metrics.
@@ -109,42 +110,50 @@ def _train_and_measure(
         tokenizer (PreTrainedTokenizerBase): Tokenizer to be used accordingly for the pretrained model.
         train_ds (Dataset): Training split of a dataset for the model to train on.
         eval_ds (Datset): Testing split of a dataset for the model to be tested on.
-        text_column (str): The column containing the text for the model to understand.
-        target_column (str): The column containing the true answers to a certain text.
-        seed (int, optional): The reproducibility seed to use for training. Defaults to 0.
-        epochs (int, optional): Amount of dataset training loops for the model to go through. Defaults to 1.
-        learning_rate (float, optional): A factor for scaling model self correction upon mistake. Defaults to 2e-5.
-        output_dir (str, optional): Directory into which the resulting tuned model weights are saved to. Defaults to "./training_output".
-        device (str, optional): The device on which the model will be fine tuned on. Defaults to "cpu".
+        config (AttackConfig): User's built attack configuration.
 
     Returns:
-        tuple[PreTrainedModel, MetricSnapshot]: _description_
+        tuple[PreTrainedModel, MetricSnapshot]: A tuple containing the new fine tuned model and its metrics.
     """
     trained_model = fine_tune(
         model=model,
         tokenizer=tokenizer,
         train_ds=train_ds,
         eval_ds=eval_ds,
-        text_column=text_column,
-        target_column=target_column,
-        seed=seed,
-        epochs=epochs,
-        learning_rate=learning_rate,
-        output_dir=output_dir,
-        device=device
+        text_column=config.text_column,
+        target_column=config.target_column,
+        seed=config.seed,
+        epochs=config.epochs,
+        learning_rate=config.learning_rate,
+        output_dir=config.output_dir,
+        device=config.device
     )
 
     metrics = capture_metrics(
-        model=model,
+        model=trained_model,
         tokenizer=tokenizer,
         dataset=eval_ds,
-        text_column=text_column,
-        target_column=target_column
+        text_column=config.text_column,
+        target_column=config.target_column
     )
 
     return trained_model, metrics
     
 
+def _split_dataset(dataset: Dataset, config: AttackConfig) -> tuple[Dataset, Dataset]:
+    """
+    Splits a given dataset based on attack configuration.
+
+    Args:
+        dataset (Dataset): A dataset to split.
+        config (AttackConfig): User's defined attack configuration.
+
+    Returns:
+        tuple[Dataset, Dataset]: A tuple containing the train split and the eval split of the dataset.
+    """
+    split = dataset.train_test_split(test_size=config.test_size, seed=config.seed)
+    train_ds, eval_ds = split["train"], split["test"]
+    return train_ds, eval_ds
 
 
 def execute(config: AttackConfig) -> RunResult:
@@ -164,6 +173,76 @@ def execute(config: AttackConfig) -> RunResult:
     Returns:
         RunResult: Final result instance holding data about the attack pipeline.
     """
-    tokenizer = AutoTokenizer.from_pretrained(config.target_model)
-    
+    _start = perf_counter()
 
+    try:
+        _attack = _build_attack(config)
+        _defense = _build_defense(config)
+    except NotImplementedError as ne:
+        return RunResult(
+            config = config,
+            status="failed",
+            error_message=str(ne)
+        )
+
+    _tokenizer = _load_tokenizer(config.target_model)
+    _dataset = _load_dataset(config)
+    _train, _eval = _split_dataset(_dataset, config)
+
+    # --- baseline model ---
+    _model_base = _load_model(config.target_model)
+
+    _, base_metrics = _train_and_measure(
+        model=_model_base,
+        tokenizer=_tokenizer,
+        train_ds=_train,
+        eval_ds=_eval,
+        config=config
+    )
+
+    # --- poisoned model ---
+    _model_poisoned = _load_model(config.target_model)
+    _train_poisoned = _attack.apply(_train)
+
+    _, poisoned_metrics = _train_and_measure(
+        model=_model_poisoned,
+        tokenizer=_tokenizer,
+        train_ds=_train_poisoned,
+        eval_ds=_eval,
+        config=config
+    )
+
+    # --- sanitized model (only if configured) ---
+    sanitized_metrics = None
+    sanitized_report = None
+
+    if config.defense_type != 'none':
+        _train_sanitized, sanitized_report = _defense(
+            dataset=_train_poisoned,
+            model=_model_poisoned,
+            tokenizer=_tokenizer,
+            target_column=config.target_column,
+            text_column=config.text_column,
+            device=config.device
+        )
+        _model_sanitized = _load_model(config.target_model)
+        
+        _, sanitized_metrics = _train_and_measure(
+            model=_model_sanitized,
+            tokenizer=_tokenizer,
+            train_ds=_train_sanitized,
+            eval_ds=_eval,
+            config=config
+        )
+
+    elapsed_time = perf_counter() - _start
+
+    return RunResult(
+        config=config,
+        status="complete",
+        baseline_metrics=base_metrics,
+        post_attack_metrics=poisoned_metrics,
+        post_defense_metrics=sanitized_metrics,
+        duration_seconds=elapsed_time,
+        filter_report=sanitized_report
+    )
